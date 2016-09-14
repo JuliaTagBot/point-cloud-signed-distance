cf76090e0eae416c5b789acea63d99c0f21f8115
Robin Deits <robin.deits@gmail.com>
2016-09-14 12:24:57 -0400

    start moving to static arrays

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 DIRECTORY=$(cd `dirname "$0"` && pwd)
+export JULIA_LOAD_PATH="$HOME/.julia"
 export JULIA_PKGDIR="$DIRECTORY/packages"
-echo "Julia package directory set to: $JULIA_PKGDIR"
+echo "Julia package directory set to: $JULIA_PKGDIR with additional packages from $JULIA_LOAD_PATH"
