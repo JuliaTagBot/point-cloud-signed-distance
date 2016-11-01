448f455a43b4d3a15b9182227b3894b8aca27656
Robin Deits <robin.deits@gmail.com>
2016-11-01 01:35:24 -0400

    implement minkowski difference with tags. it kinda works!

--- examples/gjk.jl
+++ examples/gjk.jl
@@ -5,9 +5,9 @@ import GeometryTypes
 import StaticArrays: SVector, @SVector, MVector, @MVector
 const gt = GeometryTypes
 
-immutable Tagged{P}
+immutable Tagged{P, T}
     point::P
-    tag::Int
+    tag::T
 end
 
 type AcceleratedMesh{N, T, MeshType <: gt.AbstractMesh}
@@ -57,19 +57,32 @@ function AcceleratedMesh{N, T}(mesh::gt.AbstractMesh{gt.Point{N, T}})
 end
 
 any_inside{N, T}(mesh::AcceleratedMesh{N, T}) = Tagged(gt.Vec{N, T}(gt.vertices(mesh.mesh)[1]), 1)
-any_inside(geometry) = Tagged(gt.any_inside(geometry), 0)
-any_inside{N, T}(mesh::gt.AbstractMesh{gt.Point{N, T}}) = Tagged(convert(gt.Vec{N, T}, gt.vertices(mesh)[1]), 0)
+function any_inside(geometry)
+    point = gt.any_inside(geometry)
+    Tagged(point, point)
+end
 
-function support_vector_max(geometry, direction, initial_guess::Tagged)
+function any_inside{N, T}(mesh::gt.AbstractMesh{gt.Point{N, T}})
+    point = convert(gt.Vec{N, T}, gt.vertices(mesh)[1])
+    Tagged(point, point)
+end
+
+function any_inside(m::gt.MinkowskiDifference)
+    t1 = any_inside(m.c1)
+    t2 = any_inside(m.c2)
+    Tagged(t1.point - t2.point, (t1.tag, t2.tag))
+end
+
+function support_vector_max(geometry, direction, initial_guess)
     best_pt, score = gt.support_vector_max(geometry, direction)
-    Tagged(best_pt, 0), score
+    Tagged(best_pt, best_pt), score
 end
 
 function support_vector_max{N, T}(mesh::AcceleratedMesh{N, T}, direction,
-                                  initial_guess::Tagged{gt.Vec{N, T}})
-    best = initial_guess
-    score = dot(direction, best.point)
+                                  initial_guess::Integer)
     verts = gt.vertices(mesh.mesh)
+    best = Tagged(convert(gt.Vec{N, T}, verts[initial_guess]), initial_guess)
+    score = dot(direction, best.point)
     while true
         candidates = mesh.neighbors[best.tag]
         neighbor_index, best_neighbor_score = gt.argmax(n -> dot(direction, verts[n]), candidates)
@@ -83,40 +96,58 @@ function support_vector_max{N, T}(mesh::AcceleratedMesh{N, T}, direction,
     best, score
 end
 
-function support_vector_max{N, T}(mesh::gt.HomogenousMesh{gt.Point{N, T}}, direction, initial_guess::Tagged)
+function support_vector_max{N, T}(mesh::gt.HomogenousMesh{gt.Point{N, T}}, direction, initial_guess)
     best_arg, best_value = gt.argmax(x-> x⋅direction, gt.vertices(mesh))
-    Tagged(convert(gt.Vec{N, T}, best_arg), 0), best_value
+    best_vec = convert(gt.Vec{N, T}, best_arg)
+    Tagged(best_vec, best_vec), best_value::T
 end
 
-@pure dimensionof{N, T}(::gt.AbstractGeometry{N, T}) = Val{N}()
+function support_vector_max(m::gt.MinkowskiDifference, direction, initial_guess)
+    t1, score1 = support_vector_max(m.c1, direction, initial_guess[1])
+    t2, score2 = support_vector_max(m.c2, -direction, initial_guess[2])
+    Tagged(t1.point - t2.point, (t1.tag, t2.tag)), score1 + score2
+end
+
+@pure dimensionof{N, T}(::gt.AbstractGeometry{N, T}) = Val{N}
 @pure scalartype{N, T}(::gt.AbstractGeometry{N, T}) = T
 
-@pure dimensionof{N, T, S}(::gt.AbstractSimplex{S, gt.Vec{N, T}}) = Val{N}()
+@pure dimensionof{N, T, S}(::gt.AbstractSimplex{S, gt.Vec{N, T}}) = Val{N}
 @pure scalartype{N, T, S}(::gt.AbstractSimplex{S, gt.Vec{N, T}}) = T
 
-@pure dimensionof{N, T}(::gt.HomogenousMesh{gt.Point{N, T}}) = Val{N}()
+@pure dimensionof{N, T}(::gt.HomogenousMesh{gt.Point{N, T}}) = Val{N}
 @pure scalartype{N, T}(::gt.HomogenousMesh{gt.Point{N, T}}) = T
+
+@pure dimensionof{N, T}(::gt.Vec{N, T}) = Val{N}
+@pure scalartype{N, T}(::gt.Vec{N, T}) = T
+
+@inline dimensionof(m::gt.MinkowskiDifference) = dimensionof(m.c1)
+@inline scalartype(m::gt.MinkowskiDifference) = promote_type(scalartype(m.c1), scalartype(m.c2))
 
 @inline dimensionof(mesh::AcceleratedMesh) = dimensionof(mesh.mesh)
 @inline scalartype(mesh::AcceleratedMesh) = scalartype(mesh.mesh)
 
-@pure @generated simplexlength{N}(::Val{N}) = quote
-    Val{$(N + 1)}()
+@generated simplexlength{N}(::Type{Val{N}}) = quote
+    Val{$(N + 1)}
 end
 
-typealias Simplex{M, N, T} MVector{M, Tagged{gt.Vec{N, T}}}
+typealias Simplex{M, N, T, Tag} MVector{M, Tagged{gt.Vec{N, T}, Tag}}
 
 function gjk(hull, max_iter=100, atol=1e-6)
-    gjk(hull, dimensionof(hull), scalartype(hull), simplexlength(dimensionof(hull)), max_iter, atol)
+    gjk(hull, dimensionof(hull), scalartype(hull), max_iter, atol)
 end
 
-function gjk{N, T, SimplexLength}(hull, ::Val{N}, ::Type{T}, ::Val{SimplexLength}, max_iter, atol)
+function Simplex{M, N, T, TagType}(tagged::Tagged{gt.Vec{N, T}, TagType}, simplexlen::Type{Val{M}})
+    Simplex{M, N, T, TagType}(tagged for i in 1:M)
+end
+
+function gjk{N, T}(hull, ::Type{Val{N}}, ::Type{T}, max_iter, atol)
     tagged = any_inside(hull)
-    simplex = Simplex{SimplexLength, N, T}(tagged for i in 1:SimplexLength)
+    simplex = Simplex(tagged, simplexlength(dimensionof(hull)))
+    # @code_warntype gjk(hull, simplex, tagged.point, max_iter, atol)
     gjk(hull, simplex, tagged.point, max_iter, atol)
 end
 
-function gjk{N, T, S}(hull, simplex::Simplex{S, N, T}, pt_best, max_iter=100, atol=1e-6)
+function gjk{N, T, S, Tag}(hull, simplex::Simplex{S, N, T, Tag}, pt_best, max_iter=100, atol=1e-6)
     zero_point = zero(gt.Vec{N, T})
     simplex_points = 1
     for k in 1:max_iter
@@ -125,8 +156,7 @@ function gjk{N, T, S}(hull, simplex::Simplex{S, N, T}, pt_best, max_iter=100, at
         # then do a neighbor-wise search over the mesh to try to find an even
         # better point.
         starting_vertex, _ = gt.argmax(t -> dot(t.point, direction), simplex)
-#         @code_warntype support_vector_max(hull, direction, starting_vertex)
-        improved_vertex::Tagged{gt.Vec{N, T}}, score::T = support_vector_max(hull, direction, starting_vertex)
+        improved_vertex, score = support_vector_max(hull, direction, starting_vertex.tag)
         # If we found something no better than the existing simplex, then return
         if score <= dot(pt_best, direction) + atol
             break
@@ -138,17 +168,11 @@ function gjk{N, T, S}(hull, simplex::Simplex{S, N, T}, pt_best, max_iter=100, at
                 worst_index, _ = gt.argmax(i -> -dot(simplex[i].point, direction), 1:simplex_points)
                 simplex[worst_index] = improved_vertex
             end
-            pt_best, sqd::T = gt.proj_sqdist(zero_point, gt.Simplex((convert(gt.Vec{N, T}, t.point) for t in view(simplex, 1:simplex_points))...))
-            sqd == 0 && return simplex
+            pt_best::gt.Vec{N, T}, sqd::T = gt.proj_sqdist(zero_point, gt.Simplex((convert(gt.Vec{N, T}, t.point) for t in view(simplex, 1:simplex_points))...))
+            sqd == 0 && break
         end
-
     end
     simplex, pt_best
 end
-
-# function neighbors{T}(mesh::gt.HomogenousMesh{gt.Point{3, T}}, vertex_index::Integer)
-#     neighboring_faces = filter(face -> any(gt.onebased(face, i) == vertex_index for i in 1:length(face)), gt.faces(mesh))
-#     Tagged{gt.Point{3, T}}[Tagged(gt.vertices(mesh)[face][i], gt.onebased(face, i)) for face in neighboring_faces for i in 1:length(face) if gt.onebased(face, i) != vertex_index]
-# end
 
 end
